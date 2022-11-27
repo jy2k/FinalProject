4a39a333ac6c425bd5289c3142daa5736395eeab
jy2k <jonathanyarkoni@yahoo.com>
2022-11-27 16:39:26 +0200

    replaced all colummn with 'test' string

--- main.py
+++ main.py
@@ -11,18 +11,30 @@ if __name__ == '__main__':
     print(test.year)
     print(test.month)
 
-    df = pandas.read_csv("AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo.csv", index_col='Name')
-    df.loc['Gross Coupon']
+    df = pandas.read_csv("AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo.csv", index_col=0)
+    df.index.astype(str, copy=False)
+    print(df.columns)
+    #df = df.rename_axis(["sdfsdd","Sdfdsfs","Sep 15, 2022","Aug 15, 2022","Jul 15, 2022","Jun 15, 2022","May 15, 2022","Apr 15, 2022","Mar 15, 2022","Feb 15, 2022","Jan 15, 2022","Dec 15, 2021","Nov 15, 2021","Oct 15, 2021","Sep 15, 2021","Aug 15, 2021","Jul 15, 2021","Jun 15, 2021","May 15, 2021","Apr 15, 2021","Mar 15, 2021","Feb 15, 2021","Jan 15, 2021","Dec 15, 2020","Nov 15, 2020","Oct 15, 2020","Sep 15, 2020","Jul 01, 2020"]).reset_index()
+    for val in df.columns:
+        df = df.rename(columns={val: 'test'})
+
+    #df2 = df.rename(index={'Sep 15, 2022': 'Index_3'}, inplace=True)
+
+    print(df.loc['1mo CPR'])
+    #df.index.name = 'test'
+    #print(df.index.name)
+    #df.set_index(0)
+    #df = df.query('test == 1mo CPR')
+    test= df.loc[['1mo CPR','Life CPR']]
     #print(df.columns)
-    df2 = pandas.DataFrame(data=df)
-    df2 = df2.T
-    df2['WALA'] = df2['WALA'].astype(float)
-    df2['Accum'] = df2['Accum'].replace(['-'], '0.0')
-    df2['Accum'] = df2['Accum'].astype(float)
-    print('types')
-    print(df2.dtypes)
-    print(df2['WALA'])
-    df2['spread'] = df2['WALA']-df2['Accum']
+    #df2 = pandas.DataFrame(data=test)
+    df2 = test.T
+    df2['1mo CPR'] = df2['1mo CPR'].replace(['-'], '0.0')
+    df2['1mo CPR'] = df2['1mo CPR'].astype(float)
+    df2['Life CPR'] = df2['Life CPR'].replace(['-'], '0.0')
+    df2['Life CPR'] = df2['Life CPR'].astype(float)
+
+    df2['spread'] = df2['1mo CPR']-df2['Life CPR']
 
     print('end')
 
