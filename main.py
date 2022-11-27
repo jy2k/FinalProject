dbc38233dd534c91861ae4e6a06eaba47d177e71
jy2k <jonathanyarkoni@yahoo.com>
2022-11-27 17:03:38 +0200

    working version with spread and reformated date

--- main.py
+++ main.py
@@ -4,30 +4,25 @@
 import pandas
 from datetime import datetime
 
+file_name = "AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo.csv"
+full_month_format = "%b %d, %Y"
+
 if __name__ == '__main__':
-    full_month_date = "Sep 15, 2022"
-    full_month_format = "%b %d, %Y"
-    test = datetime.strptime(full_month_date, full_month_format)
-    print(test.year)
-    print(test.month)
 
-    df = pandas.read_csv("AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo.csv", index_col=0)
+    df = pandas.read_csv(file_name, index_col=0)
     df.index.astype(str, copy=False)
-    print(df.columns)
-    #df = df.rename_axis(["sdfsdd","Sdfdsfs","Sep 15, 2022","Aug 15, 2022","Jul 15, 2022","Jun 15, 2022","May 15, 2022","Apr 15, 2022","Mar 15, 2022","Feb 15, 2022","Jan 15, 2022","Dec 15, 2021","Nov 15, 2021","Oct 15, 2021","Sep 15, 2021","Aug 15, 2021","Jul 15, 2021","Jun 15, 2021","May 15, 2021","Apr 15, 2021","Mar 15, 2021","Feb 15, 2021","Jan 15, 2021","Dec 15, 2020","Nov 15, 2020","Oct 15, 2020","Sep 15, 2020","Jul 01, 2020"]).reset_index()
+
     for val in df.columns:
-        df = df.rename(columns={val: 'test'})
+        if(val != 'Unnamed: 1'):
+            interim = datetime.strptime(val, full_month_format)
+            month = str(interim.month)
+            year = str(interim.year)
+            new_format = str(month + '/' + year)
+            df = df.rename(columns={val: new_format})
 
-    #df2 = df.rename(index={'Sep 15, 2022': 'Index_3'}, inplace=True)
-
-    print(df.loc['1mo CPR'])
-    #df.index.name = 'test'
-    #print(df.index.name)
-    #df.set_index(0)
-    #df = df.query('test == 1mo CPR')
+    #select specific columns
     test= df.loc[['1mo CPR','Life CPR']]
-    #print(df.columns)
-    #df2 = pandas.DataFrame(data=test)
+
     df2 = test.T
     df2['1mo CPR'] = df2['1mo CPR'].replace(['-'], '0.0')
     df2['1mo CPR'] = df2['1mo CPR'].astype(float)
