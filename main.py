ed7928c1e71fe45ffc1388466f50b940d777517f
jy2k <jonathanyarkoni@yahoo.com>
2022-11-28 11:21:28 +0200

    Cleaned the code

--- main.py
+++ main.py
@@ -4,62 +4,66 @@
 import pandas
 from datetime import datetime
 
-file_name = "AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo"
-full_month_format = "%b %d, %Y"
+###### Params ######
+
+file_1 = "AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo"
+full_month_format_file_1 = "%b %d, %Y"
+
+file_2 = "AFRM"
+full_month_format_file_2 = "%Y-%m-%d"
+
 list_of_params = ['Gross Coupon', 'Accum Net Loss%', 'Annualized Net Loss Rate', 'Delinq 30+', 'Number of Assets', 'Life CDR', 'Life CPR']
+
+
+###### Functions ######
+
+def bucketize_date(val, full_month_format):
+    interim = datetime.strptime(val, full_month_format)
+    month = str(interim.month)
+    year = str(interim.year)
+    new_format = str(month + '/' + year)
+    return new_format
 
 if __name__ == '__main__':
 
-    df = pandas.read_csv(str(file_name+'.csv'), index_col=0)
-    df.index.astype(str, copy=False)
+    ###### Cohort file ######
+    df_cohort = pandas.read_csv(str(file_1+'.csv'), index_col=0)
+    df_cohort.index.astype(str, copy=False)
 
-    for val in df.columns:
+    for val in df_cohort.columns:
         if(val != 'Unnamed: 1'):
-            new_format = datetime.strptime(val, full_month_format)
-            #interim = datetime.strptime(val, full_month_format)
-            #month = str(interim.month)
-            #year = str(interim.year)
-            #new_format = str(month + '/' + year)
-            df = df.rename(columns={val: new_format})
+            new_format = datetime.strptime(val, full_month_format_file_1)
+            df_cohort = df_cohort.rename(columns={val: new_format})
 
     #select specific columns
-    test= df.loc[list_of_params]
-    test2 = test[~test.index.duplicated(keep='first')]
-    df2 = test2.T
-    df2 = df2.drop("Unnamed: 1")
+    df_cohort = df_cohort.loc[list_of_params]
+    df_cohort = df_cohort[~df_cohort.index.duplicated(keep='first')]
+    df_cohort = df_cohort.T
+    df_cohort = df_cohort.drop("Unnamed: 1")
 
     for param in list_of_params:
-        df2[param] = df2[param].replace(['-'], '0.0')
-        df2[param] = df2[param].str.replace(',', '')
-        df2[param] = df2[param].astype(float)
+        df_cohort[param] = df_cohort[param].replace(['-'], '0.0')
+        df_cohort[param] = df_cohort[param].str.replace(',', '')
+        df_cohort[param] = df_cohort[param].astype(float)
 
-    df2['Gross Coupon - Accum Net Loss%'] = df2['Gross Coupon'] - df2['Accum Net Loss%']
-    df2['Num Assets in Delinq 30+ Days / Number of Assets'] = df2['Delinq 30+'] / df2['Number of Assets']
+    df_cohort['Gross Coupon - Accum Net Loss%'] = df_cohort['Gross Coupon'] - df_cohort['Accum Net Loss%']
+    df_cohort['Num Assets in Delinq 30+ Days / Number of Assets'] = df_cohort['Delinq 30+'] / df_cohort['Number of Assets']
 
-    df2.to_csv(str(file_name+'_reformatted.csv'))
+    df_cohort.to_csv(str(file_1+'_reformatted.csv'))
 
     ###### Stock file ######
-    file_name = "AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo" + '_reformatted.csv'
-    file_name_stock = "AFRM"
 
-    df3 = pandas.read_csv(str(file_name_stock + '.csv'), index_col=0)
-    df3 = df3.T
+    df_stock = pandas.read_csv(str(file_2 + '.csv'), index_col=0)
+    df_stock = df_stock.T
 
-    full_month_format = "%Y-%m-%d"
-
-    for val in df3.columns:
+    for val in df_stock.columns:
         if (val != 'Date'):
-            new_format = datetime.strptime(val, full_month_format)
-            #interim = datetime.strptime(val, full_month_format)
-            #month = str(interim.month)
-            #year = str(interim.year)
-            #new_format = str(month + '/' + year)
-            df3 = df3.rename(columns={val: new_format})
-    test = df3.T
-    print(test.index.name)
-    print(df2.index.name)
+            new_format = datetime.strptime(val, full_month_format_file_2)
+            df_stock = df_stock.rename(columns={val: new_format})
 
-    result = pandas.merge(df2, test, left_index=True, right_index=True, how='outer')
+    df_stock = df_stock.T
+
+    df_cohort_stock = pandas.merge(df_cohort, df_stock, left_index=True, right_index=True, how='outer')
 
     print('end')
 
