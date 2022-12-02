f7b8c322ac0350faca635a8c14e01dc118324261
jy2k <jonathanyarkoni@yahoo.com>
2022-12-02 12:56:57 +0200

    Added 3 Affirm files

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ full_month_format_file_2 = "%Y-%m-%d"
 file_3 = "Finsight data - Affirm_edited"
 full_month_format_file_3 = "%B %d, %Y"
 
-list_of_params = ['1mo CPR','Gross Coupon', 'Accum Net Loss%', 'Annualized Net Loss Rate', 'Delinq 30+', 'Number of Assets', 'Life CDR', 'Life CPR']
-columns_names = ['Deal','Gross_Coupon','Accum_Net_Loss_precent','Annualized_Net_Loss_Rate','Delinq_30','Number_of_Assets','Life_CDR','Life_CPR','Gross_Coupon_Accum_Net_Loss_precent','Num_Assets_in_Delinq_30_Days_Number_of_Assets','Open','High','Low','Close','Adj_Close','Volume']
+list_of_params = ['1mo CPR','Gross Coupon', 'Annualized Net Loss Rate', 'Delinq 30+', 'Number of Assets', 'Life CDR', ]
+columns_names = ['Deal','Gross_Coupon','Annualized_Net_Loss_Rate','Delinq_30','Number_of_Assets','Life_CDR','Num_Assets_in_Delinq_30_Days_Number_of_Assets','Open','High','Low','Close','Adj_Close','Volume']
 
 list_of_affirm_cohort = ['cohort/Affirm/20-1/AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo.csv',
                          'cohort/Affirm/20-2/AFRM20Z2_20221011.xlsx - AFRM20Z2-HistInfo.csv',
@@ -78,7 +78,7 @@ if __name__ == '__main__':
     df_cohort['1mo CPR'] = df_cohort['1mo CPR'].astype("string")
     df_cohort['1mo CPR'] = deal
 
-    df_cohort['Gross Coupon - Accum Net Loss%'] = df_cohort['Gross Coupon'] - df_cohort['Accum Net Loss%']
+   #df_cohort['Gross Coupon - Accum Net Loss%'] = df_cohort['Gross Coupon'] - df_cohort['Accum Net Loss%']
     df_cohort['Num Assets in Delinq 30+ Days / Number of Assets'] = df_cohort['Delinq 30+'] / df_cohort['Number of Assets']
 
     #df_cohort.to_csv(str(file_1+'_reformatted.csv'))
