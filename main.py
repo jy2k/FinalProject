8309f1b8ed1ce7c61d1fc69b85ac12c58eededba
jy2k <jonathanyarkoni@yahoo.com>
2022-12-02 11:21:44 +0200

    Added Deal as a metadata column

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from datetime import datetime
 file_1 = "AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo"
 full_month_format_file_1 = "%b %d, %Y"
 
-file_2 = "AFRM"
+file_2 = "stocks/AFRM"
 full_month_format_file_2 = "%Y-%m-%d"
 
 file_3 = "Finsight data - Affirm_edited"
 full_month_format_file_3 = "%B %d, %Y"
 
-list_of_params = ['Gross Coupon', 'Accum Net Loss%', 'Annualized Net Loss Rate', 'Delinq 30+', 'Number of Assets', 'Life CDR', 'Life CPR']
-columns_names = ['Gross_Coupon','Accum_Net_Loss_precent','Annualized_Net_Loss_Rate','Delinq_30','Number_of_Assets','Life_CDR','Life_CPR','Gross_Coupon_Accum_Net_Loss_precent','Num_Assets_in_Delinq_30_Days_Number_of_Assets','Open','High','Low','Close','Adj_Close','Volume']
+list_of_params = ['1mo CPR','Gross Coupon', 'Accum Net Loss%', 'Annualized Net Loss Rate', 'Delinq 30+', 'Number of Assets', 'Life CDR', 'Life CPR']
+columns_names = ['Deal','Gross_Coupon','Accum_Net_Loss_precent','Annualized_Net_Loss_Rate','Delinq_30','Number_of_Assets','Life_CDR','Life_CPR','Gross_Coupon_Accum_Net_Loss_precent','Num_Assets_in_Delinq_30_Days_Number_of_Assets','Open','High','Low','Close','Adj_Close','Volume']
 
 list_of_affirm_cohort = ['cohort/Affirm/20-1/AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo.csv',
                          'cohort/Affirm/20-2/AFRM20Z2_20221011.xlsx - AFRM20Z2-HistInfo.csv',
@@ -24,7 +24,7 @@ def concat_csvs(list_csvs):
     df_full = pandas.DataFrame()
 
     for csv in list_csvs:
-        current = pandas.read_csv(csv)
+        current = pandas.read_csv(csv, index_col=0)
         df_full = df_full.append(current)
 
     return df_full
@@ -37,31 +37,46 @@ def bucketize_date(val, full_month_format):
     return new_format
 
 if __name__ == '__main__':
-    df_cohort = concat_csvs(list_of_affirm_cohort)
-    ###### Cohort file ######
-    df_cohort = pandas.read_csv(str(file_1+'.csv'), index_col=0)
-    df_cohort.index.astype(str, copy=False)
 
+    ###### Cohort file ######
+    df_cohort = concat_csvs(list_of_affirm_cohort)
+
+    df_cohort.index.astype(str, copy=False)
+    deal = df_cohort.loc['WALA']['Graph'][0]
+    print(deal)
     for val in df_cohort.columns:
-        if(val != 'Unnamed: 1'):
+        if(val != 'Unnamed: 1' and val !='Graph'):
             new_format = datetime.strptime(val, full_month_format_file_1)
             df_cohort = df_cohort.rename(columns={val: new_format})
 
     #select specific columns
     df_cohort = df_cohort.loc[list_of_params]
     df_cohort = df_cohort[~df_cohort.index.duplicated(keep='first')]
+    try:
+        df_cohort = df_cohort.drop("Graph", axis=1)
+    except:
+        print("Graph does not exist")
+
     df_cohort = df_cohort.T
-    df_cohort = df_cohort.drop("Unnamed: 1")
+
+    try:
+        df_cohort = df_cohort.drop("Unnamed: 1")
+    except:
+        print("unnamed: 1 does not exist")
+
 
     for param in list_of_params:
         df_cohort[param] = df_cohort[param].replace(['-'], '0.0')
         df_cohort[param] = df_cohort[param].str.replace(',', '')
         df_cohort[param] = df_cohort[param].astype(float)
 
+    df_cohort['1mo CPR'] = df_cohort['1mo CPR'].astype("string")
+    df_cohort['1mo CPR'] = deal
+
     df_cohort['Gross Coupon - Accum Net Loss%'] = df_cohort['Gross Coupon'] - df_cohort['Accum Net Loss%']
     df_cohort['Num Assets in Delinq 30+ Days / Number of Assets'] = df_cohort['Delinq 30+'] / df_cohort['Number of Assets']
 
-    df_cohort.to_csv(str(file_1+'_reformatted.csv'))
+    #df_cohort.to_csv(str(file_1+'_reformatted.csv'))
 
     ###### Stock file ######
 
@@ -83,7 +98,7 @@ if __name__ == '__main__':
 
     df_cohort_stock = df_cohort_stock.dropna()
 
-    df_cohort_stock.to_csv('final.csv')
+    df_cohort_stock.to_csv('data/cohort stock/Affirm/Affirm-cohort-stock.csv')
 
     print('end')
 
