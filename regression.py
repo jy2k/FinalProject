148461c6895fa3063adbb2ee33a15f0d610e27b9
jy2k <jonathanyarkoni@yahoo.com>
2022-12-02 11:50:43 +0200

    Added regression on single column

---
+++ regression.py
@@ -0,0 +1,20 @@
+from sklearn import linear_model, metrics, model_selection
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+df = pd.read_csv('data/cohort stock/Affirm/Affirm-cohort-stock.csv', index_col=0)
+
+X = df['Gross_Coupon'].values.reshape(-1, 1)
+y = df['Close'].values.reshape(-1, 1)
+
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.2)
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+print(regressor.intercept_)
+print(regressor.coef_)
+
+score = regressor.predict([[9.5]])
+print(score) # 94.80663482
+
+print('end')
