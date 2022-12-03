57732b3e006e7b56b3c66dc378acc62d234f14c5
jy2k <jonathanyarkoni@yahoo.com>
2022-12-03 15:54:36 +0200

    Changed merge_finsight_stock to work with dict

--- merge_finsight_stock.py
+++ merge_finsight_stock.py
@@ -1,14 +1,21 @@
-from sklearn import linear_model, metrics, model_selection
-from sklearn.linear_model import LinearRegression
 import pandas as pd
 
-lc_finsight_df = pd.read_csv('/Users/eyalben-eliyahu/PycharmProjects/FinalProject/Finsight/Finsight data - LC_edited_reformatted.csv', index_col=0)
-lc_stock_df = pd.read_csv('/Users/eyalben-eliyahu/PycharmProjects/FinalProject/stocks/LC.csv')
+dict_stocks_finsight = {'AFRM': 'Finsight/Finsight data - Affirm_edited_reformatted.csv',
+        'LC': 'Finsight/Finsight data - LC_edited_reformatted.csv',
+        'OPRT': 'Finsight/Finsight data - OPRT_edited_reformatted.csv',
+        'SOFI': 'Finsight/Finsight data - SoFi_edited_onlyESOT_reformatted.csv',
+        'UPST': 'Finsight/Finsight data - UPST_edited_reformatted.csv'
+}
 
-lc_finsight_stocks_df = lc_finsight_df.merge(lc_stock_df, left_index=True, right_on = 'Date')
-lc_finsight_stocks_df = lc_finsight_stocks_df.set_index('Date')
+for stock, finsight_file in dict_stocks_finsight.items():
 
-lc_finsight_stocks_df.to_csv("lc_finsight_stocks_df")
+    lc_finsight_df = pd.read_csv(finsight_file, index_col=0)
+    lc_stock_df = pd.read_csv(f'stocks/{stock}.csv')
+
+    lc_finsight_stocks_df = lc_finsight_df.merge(lc_stock_df, left_index=True, right_on = 'Date')
+    lc_finsight_stocks_df = lc_finsight_stocks_df.set_index('Date')
+
+    lc_finsight_stocks_df.to_csv(f'data/finsight/{stock}/file1.csv')
 
 
 
