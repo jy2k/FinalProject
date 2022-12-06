a2e00cbcdbeb2bafde45fe237e93b14117ecc5bf
jy2k <jonathanyarkoni@yahoo.com>
2022-12-06 19:28:10 +0200

    Fixed 2 outstanding files with odd column names

--- merge_cohort_stock.py
+++ merge_cohort_stock.py
@@ -5,18 +5,18 @@ from datetime import datetime
 # https://www.programiz.com/python-programming/datetime/strptime
 
 list_of_params = ['1mo CPR','Gross Coupon', 'Accum Net Loss%', 'Annualized Net Loss Rate', 'Delinq 30+', 'Number of Assets', 'Life CDR']
-columns_names = ['Deal','Gross_Coupon','Accum_Net_Loss_precent','Annualized_Net_Loss_Rate','Delinq_30','Number_of_Assets','Life_CDR','Gross_Coupon_Accum_Net_Loss_precent','Num_Assets_in_Delinq_30_Days_Number_of_Assets','Open','High','Low','Close','Adj_Close','Volume']
+stock_column_names = ['Open','High','Low','Close','Adj_Close','Volume']
 
 list_of_affirm_cohort = ['cohort/AFRM/20-1/AFRM20Z1_20221011.xlsx - AFRM20Z1-HistInfo.csv',
-                         'cohort/AFRM/20-2/AFRM20Z2_20221011.xlsx - AFRM20Z2-HistInfo.csv',]
-                         #'cohort/AFRM/21-1/AFRM21A_20221011.xlsx - AFRM21A-HistInfo.csv'] # no accum net loss %
+                         'cohort/AFRM/20-2/AFRM20Z2_20221011.xlsx - AFRM20Z2-HistInfo.csv',
+                         'cohort/AFRM/21-1/AFRM21A_20221011.xlsx - AFRM21A-HistInfo.csv'] # no accum net loss %
 
 list_of_lendingclub_cohort = ['cohort/LC/19-1/LCR191_20220928.xlsx - LCR191-CStats.csv',
                          'cohort/LC/20-1/LCR201_20220928(2).xlsx - LCR201-CStats.csv',
                          'cohort/LC/21-1/LCLC21N1_20220928.xlsx - LCLC21N1-CStats.csv']
 
 list_of_oportun_cohort = ['cohort/OPRT/19-1/OPF1319A_20221011.xlsx - OPF1319A-HistInfo.csv',
-                         #'cohort/OPRT/20-1/OPF2001_20221011.xlsx - OPF2001-HistInfo.csv', # ['Gross Coupon', 'Number of Assets'] not in index
+                         'cohort/OPRT/20-1/OPF2001_20221011.xlsx - OPF2001-HistInfo.csv', # ['Gross Coupon', 'Number of Assets'] not in index
                          'cohort/OPRT/21-1/OPF21A_20221011.xlsx - OPF21A-HistInfo.csv']
 
 list_of_sofi_cohort = ['cohort/SOFI/19-4/SFLP1904_20221011.xlsx - SFLP1904-HistInfo.csv',
@@ -28,14 +28,14 @@ list_of_upst_cohort = ['cohort/UPST/20-1/UPSP20S1_20220928.xlsx - UPSP20S1-CStat
                          'cohort/UPST/22-1P/UPSP22P1_20220928.xlsx - UPSP22P1-CStats.csv',
                          'cohort/UPST/22-1S/UPSP22S1_20220928.xlsx - UPSP22S1-CStats.csv']
 
-dict_stocks_finsight = {'AFRM': { 'files_cohort' : list_of_affirm_cohort, 'date_format' : "%b %d, %Y", 'day_of_the_month' : 15},
+dict_cohort_files = {'AFRM': {'files_cohort' : list_of_affirm_cohort, 'date_format' : "%b %d, %Y", 'day_of_the_month' : 15},
                          'LC': { 'files_cohort' : list_of_lendingclub_cohort, 'date_format' : "%b %y", 'day_of_the_month' : 1},
                         'OPRT': { 'files_cohort' : list_of_oportun_cohort, 'date_format' : "%b %d, %Y", 'day_of_the_month' : 8},
                         'SOFI': { 'files_cohort' : list_of_sofi_cohort, 'date_format' : "%b %d, %Y", 'day_of_the_month' : 25},
                         'UPST': { 'files_cohort' : list_of_upst_cohort, 'date_format' : "%b %y", 'day_of_the_month' : 1}}
 
 
-def work(filename, stock, date_format, day_of_the_month):
+def work(filename, stock, date_format, day_of_the_month,column_params):
 
     ###### Cohort file ######
     df_cohort = pandas.read_csv(filename, index_col=0)
@@ -54,7 +54,7 @@ def work(filename, stock, date_format, day_of_the_month):
 
     print(str('working on: '+ filename))
     #Select specific columns
-    df_cohort = df_cohort.loc[list_of_params]
+    df_cohort = df_cohort.loc[column_params]
     df_cohort = df_cohort[~df_cohort.index.duplicated(keep='first')]
 
     try: df_cohort = df_cohort.drop("Graph", axis=1)
@@ -68,16 +68,16 @@ def work(filename, stock, date_format, day_of_the_month):
 
     #Change format to float
     #Replace missing values
-    for param in list_of_params:
+    for param in column_params:
         df_cohort[param] = df_cohort[param].replace(['-'], '0.0')
-        df_cohort[param] = df_cohort[param].str.replace(',', '')
+        df_cohort[param] = df_cohort[param].astype(str).str.replace(',', '')
         df_cohort[param] = df_cohort[param].astype(float)
 
     df_cohort['1mo CPR'] = df_cohort['1mo CPR'].astype("string")
     df_cohort['1mo CPR'] = deal
 
-    df_cohort['Gross Coupon - Accum Net Loss%'] = df_cohort['Gross Coupon'] - df_cohort['Accum Net Loss%']
-    df_cohort['Num Assets in Delinq 30+ Days / Number of Assets'] = df_cohort['Delinq 30+'] / df_cohort['Number of Assets']
+    #df_cohort['Gross Coupon - Accum Net Loss%'] = df_cohort['Gross Coupon'] - df_cohort['Accum Net Loss%']
+    #df_cohort['Num Assets in Delinq 30+ Days / Number of Assets'] = df_cohort['Delinq 30+'] / df_cohort['Number of Assets']
 
     #df_cohort.to_csv(str(file_1+'_reformatted.csv'))
 
@@ -90,7 +90,7 @@ def work(filename, stock, date_format, day_of_the_month):
 
     df_cohort_stock = pandas.merge(df_cohort, df_stock_filtered_final, left_index=True, right_index=True, how='outer')
 
-    df_cohort_stock.columns = columns_names
+    df_cohort_stock.columns = column_params+stock_column_names
     df_cohort_stock.index.name = 'Date'
     #df_cohort_stock['series'] = 1
 
@@ -98,12 +98,21 @@ def work(filename, stock, date_format, day_of_the_month):
 
     return df_cohort_stock
 
-for stock,value in dict_stocks_finsight.items():
-    #print(key, value)
+for stock,value in dict_cohort_files.items():
     print(value)
     i=0
     for file in value['files_cohort']:
         print(file)
-        df_cohort_stock = work(stock=stock, filename=file, day_of_the_month=value['day_of_the_month'], date_format=value['date_format'])
+        current_day_of_the_month = value['day_of_the_month']
+
+        if file == 'cohort/AFRM/21-1/AFRM21A_20221011.xlsx - AFRM21A-HistInfo.csv':
+            current_list = ['1mo CPR','Gross Coupon', 'Annualized Net Loss Rate', 'Delinq 30+', 'Number of Assets', 'Life CDR']
+        elif file ==  'cohort/OPRT/20-1/OPF2001_20221011.xlsx - OPF2001-HistInfo.csv':
+            current_list = ['1mo CPR','Gross Coupon (Derived)', 'Annualized Net Loss Rate', 'Delinq 30+', 'Life CDR']
+            current_day_of_the_month = 15
+        else:
+            current_list = list_of_params
+
+        df_cohort_stock = work(stock=stock, filename=file, day_of_the_month=current_day_of_the_month, date_format=value['date_format'], column_params=current_list)
         df_cohort_stock.to_csv(f'data/cohort stock/{stock}/file_{i}.csv')
         i+=1
