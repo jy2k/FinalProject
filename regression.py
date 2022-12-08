22731538d098a31a9626708ab47c9fafa8b65b1f
jy2k <jonathanyarkoni@yahoo.com>
2022-12-08 15:47:52 +0200

    Changed regression to OLS

--- regression.py
+++ regression.py
@@ -1,20 +1,12 @@
-from sklearn import linear_model, metrics, model_selection
-from sklearn.linear_model import LinearRegression
+
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
-df = pd.read_csv('data/cohort stock/AFRM/AFRM-cohort-stock.csv', index_col=0)
+df = pd.read_csv('data/cohort stock/AFRM/file_0.csv', index_col=0)
 
-X = df['Gross_Coupon'].values.reshape(-1, 1)
-y = df['Close'].values.reshape(-1, 1)
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.2)
-
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
-print(regressor.intercept_)
-print(regressor.coef_)
-
-score = regressor.predict([[9.5]])
-print(score) # 94.80663482
-
+results = smf.ols('Close ~ Low', data=df).fit()
+print(results.summary())
 print('end')
