7588797e1bb1e2f3c4041dca2e6ba15e5f668c63
jy2k <jonathanyarkoni@yahoo.com>
2022-12-18 19:19:16 +0200

    Training XG boost with RMSE metric

---
+++ run_xgboost.py
@@ -0,0 +1,22 @@
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+
+dataset = pd.read_csv('Output data/cohort stock/AFRM/file_0_benchmark.csv')
+
+dataset.dropna(axis=0, subset=['adj_1_change'], inplace=True)
+y = dataset['adj_1_change'].astype('float')
+X = dataset[['Life CDR','Open']].astype('float')
+
+train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.25)
+
+xgbr = XGBRegressor(objective='reg:squarederror')
+xgbr.fit(train_X, train_y)
+ypred = xgbr.predict(test_X)
+mse = mean_squared_error(test_y, ypred)
+
+print("RMSE: %.2f" % (mse ** (1 / 2.0)))
+
+# Getting r_2_score
+# https://scikit-learn.org/stable/modules/model_evaluation.html#r2-score-the-coefficient-of-determination
