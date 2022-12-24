041be75ae9412da5168d08d3e7b7b5a320a619ec
jy2k <jonathanyarkoni@yahoo.com>
2022-12-24 12:58:50 +0200

    Cleaned and organized cohort_stock

--- Cohort_stock reg.py
+++ Cohort_stock reg.py
@@ -1,301 +1,100 @@
-import pandas as pd
-import statsmodels.formula.api as sm
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import statsmodels.formula.api as smf
-from statsmodels.compat import lzip
-import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from statsmodels.formula.api import ols
+from sklearn.model_selection import train_test_split
+import pandas as pd
 
-features_in_data = ['1mo_CPR', 'Gross_Coupon', 'Accum_Net_Loss%',
-       'Annualized_Net_Loss_Rate', 'Delinq_30', 'Number_of_Assets',
-       'Life_CDR', 'Open', 'High', 'Low', 'Close', 'Adj_Close', 'Volume',
-       'Monthly_Change']
+def add_columns(df):
+    df.columns = df.columns.str.replace(' ', '_')
+    df.columns = df.columns.str.replace('+', '')
+    df['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'] = df['Gross_Coupon'].subtract(df['Annualized_Net_Loss_Rate'])
+    df['d1_dist_from_bench'] = df['adj_1_change'] - df['avg_adj_1_change']
+    df['d7_dist_from_bench'] = df['adj_7_change'] - df['avg_adj_7_change']
+    df['d30_dist_from_bench'] = df['adj_30_change'] - df['avg_adj_30_change']
+    df['d90_dist_from_bench'] = df['adj_90_change'] - df['avg_adj_90_change']
+    return df
+# add ability to control adj/unadj
+# add ability to control out of sample size
+def my_ols(df, X_list, y_list):
 
-### AFRM ########################################## AFRM ######################################## AFRM ################
+    in_sample_r2 = []
+    out_of_sample_r2 = []
+
+    for i in y_list:
+        y = df_combined_stocks[i]
+        X = df_combined_stocks[X_list]
+        train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.2)
+        results = sm.OLS(train_y, train_X).fit()
+        from sklearn.metrics import r2_score
+        ypred = results.predict(test_X)
+        r2 = r2_score(test_y, ypred)
+        #print(f'R2: {r2}')
+        #print(results.summary())
+
+        out_of_sample_r2.append(r2)
+        in_sample_r2.append(results.rsquared)
+
+    print('In of sample: ')
+    print(in_sample_r2)
+    print('Out of sample: ')
+    print(out_of_sample_r2)
+
+    return in_sample_r2, out_of_sample_r2
+
+def plot_r2(r2_change_list, r2_bench_list):
+    x = ['1d', '7d', '30d', '90d']
+
+    plt.plot(x, r2_change_list, label ="stock change", color ="blue")
+    plt.plot(x, r2_bench_list, label ="Benchmark", color ="green")
+    plt.title('AFRM R^2 for % change and Benchmark in stock')
+    plt.ylabel('R^2')
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+
+##################### Run Regression #####################
 
 afrm_cohort_stock1 = pd.read_csv(f'Output data/cohort stock/AFRM/file_0_benchmark.csv')
 afrm_cohort_stock2 = pd.read_csv(f'Output data/cohort stock/AFRM/file_1_benchmark.csv')
 afrm_cohort_stock3 = pd.read_csv(f'Output data/cohort stock/AFRM/file_2_benchmark.csv')
-
-afrm_deals = [afrm_cohort_stock1, afrm_cohort_stock2] #afrm_cohort_stock3
-
-for df in afrm_deals:
-    df.columns = df.columns.str.replace(' ', '_')
-    df.columns = df.columns.str.replace('+', '')
-    df['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'] = df['Gross_Coupon'].subtract(df['Annualized_Net_Loss_Rate'])
-    df['d1_dist_from_bench'] = df['adj_1_change'] - df['avg_adj_1_change']
-    df['d7_dist_from_bench'] = df['adj_7_change'] - df['avg_adj_7_change']
-    df['d30_dist_from_bench'] = df['adj_30_change'] - df['avg_adj_30_change']
-    df['d90_dist_from_bench'] = df['adj_90_change'] - df['avg_adj_90_change']
-
-afrm_cohort_stock_combined = pd.concat(afrm_deals, ignore_index=True, sort=False)
-
-lc_costock_combine_bench_rsquared = []
-for i in ['d1_dist_from_bench', 'd7_dist_from_bench', 'd30_dist_from_bench', 'd90_dist_from_bench']:
-    results = smf.ols(f'{i} ~ Annualized_Net_Loss_Rate + Life_CDR', data=afrm_cohort_stock_combined).fit()
-    print(results.summary())
-    lc_costock_combine_bench_rsquared.append(results.rsquared)
-print(lc_costock_combine_bench_rsquared)
-
-lc_costock_combine_change_rsquared = []
-for i in ['adj_1_change', 'adj_7_change', 'adj_30_change', 'adj_90_change']:
-    results = smf.ols(f'{i} ~ Annualized_Net_Loss_Rate + Life_CDR', data=afrm_cohort_stock_combined).fit()
-    print(results.summary())
-    lc_costock_combine_change_rsquared.append(results.rsquared)
-print(lc_costock_combine_change_rsquared)
-print(lc_costock_combine_bench_rsquared)
-
-
-x = ['1d', '7d', '30d', '90d']
-
-plt.plot(x, lc_costock_combine_change_rsquared, label ="stock change", color ="blue")
-plt.plot(x, lc_costock_combine_bench_rsquared, label ="Benchmark", color ="green")
-plt.title('AFRM R^2 for % change and Benchmark in stock')
-plt.ylabel('R^2')
-plt.grid()
-plt.legend()
-plt.show()
-
-for i in afrm_deals:
-    plt.plot(i['Annualized_Net_Loss_Rate'])
-    plt.show()
-
-### LC ########################################## LC ######################################## LC ##################
+afrm_files = [afrm_cohort_stock1, afrm_cohort_stock2, afrm_cohort_stock3]
 
 lc_cohort_stock1 = pd.read_csv(f'Output data/cohort stock/LC/file_0_benchmark.csv')
 lc_cohort_stock2 = pd.read_csv(f'Output data/cohort stock/LC/file_1_benchmark.csv')
 lc_cohort_stock3 = pd.read_csv(f'Output data/cohort stock/LC/file_2_benchmark.csv')
-
-lc_deals = [lc_cohort_stock1,lc_cohort_stock3] # lc_cohort_stock2
-
-for df in lc_deals:
-    df.columns = df.columns.str.replace(' ', '_')
-    df.columns = df.columns.str.replace('+', '')
-    df['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'] = df['Gross_Coupon'].subtract(df['Annualized_Net_Loss_Rate'])
-    df['d1_dist_from_bench'] = df['adj_1_change'] - df['avg_adj_1_change']
-    df['d7_dist_from_bench'] = df['adj_7_change'] - df['avg_adj_7_change']
-    df['d30_dist_from_bench'] = df['adj_30_change'] - df['avg_adj_30_change']
-    df['d90_dist_from_bench'] = df['adj_90_change'] - df['avg_adj_90_change']
-
-lc_cohort_stock_combined = pd.concat(lc_deals, ignore_index=True, sort=False)
-
-lc_costock_combine_bench_rsquared = []
-for i in ['d1_dist_from_bench', 'd7_dist_from_bench', 'd30_dist_from_bench', 'd90_dist_from_bench']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=lc_cohort_stock_combined).fit()
-    print(results.summary())
-    lc_costock_combine_bench_rsquared.append(results.rsquared)
-print(lc_costock_combine_bench_rsquared)
-
-lc_costock_combine_change_rsquared = []
-for i in ['adj_1_change', 'adj_7_change', 'adj_30_change', 'adj_90_change']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=lc_cohort_stock_combined).fit()
-    print(results.summary())
-    lc_costock_combine_change_rsquared.append(results.rsquared)
-print(lc_costock_combine_change_rsquared)
-print(lc_costock_combine_bench_rsquared)
-
-
-x = ['1d', '7d', '30d', '90d']
-
-plt.plot(x, lc_costock_combine_change_rsquared, label ="stock change", color ="blue")
-plt.plot(x, lc_costock_combine_bench_rsquared, label ="Benchmark", color ="green")
-plt.title('LC R^2 for % change and Benchmark in stock')
-plt.ylabel('R^2')
-plt.grid()
-plt.legend()
-plt.show()
-
-for i in lc_deals:
-    plt.plot(i['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'])
-    plt.show()
-
-
-### OPRT ########################################## OPRT ######################################## OPRT ##################
+lc_files = [lc_cohort_stock1,lc_cohort_stock3] # lc_cohort_stock2
 
 oprt_cohort_stock1 = pd.read_csv(f'Output data/cohort stock/OPRT/file_0_benchmark.csv')
 oprt_cohort_stock2 = pd.read_csv(f'Output data/cohort stock/OPRT/file_1_benchmark.csv')
 oprt_cohort_stock3 = pd.read_csv(f'Output data/cohort stock/OPRT/file_2_benchmark.csv')
-
-oprt_deals = [oprt_cohort_stock1, oprt_cohort_stock3] #oprt_cohort_stock2
-
-for df in oprt_deals:
-    df.columns = df.columns.str.replace(' ', '_')
-    df.columns = df.columns.str.replace('+', '')
-    df['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'] = df['Gross_Coupon'].subtract(df['Annualized_Net_Loss_Rate'])
-    df['d1_dist_from_bench'] = df['adj_1_change'] - df['avg_adj_1_change']
-    df['d7_dist_from_bench'] = df['adj_7_change'] - df['avg_adj_7_change']
-    df['d30_dist_from_bench'] = df['adj_30_change'] - df['avg_adj_30_change']
-    df['d90_dist_from_bench'] = df['adj_90_change'] - df['avg_adj_90_change']
-
-oprt_cohort_stock_combined = pd.concat(oprt_deals, ignore_index=True, sort=False)
-
-oprt_costock_combine_bench_rsquared = []
-for i in ['d1_dist_from_bench', 'd7_dist_from_bench', 'd30_dist_from_bench', 'd90_dist_from_bench']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=oprt_cohort_stock_combined).fit()
-    print(results.summary())
-    oprt_costock_combine_bench_rsquared.append(results.rsquared)
-print(oprt_costock_combine_bench_rsquared)
-
-oprt_costock_combine_change_rsquared = []
-for i in ['adj_1_change', 'adj_7_change', 'adj_30_change', 'adj_90_change']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=oprt_cohort_stock_combined).fit()
-    print(results.summary())
-    oprt_costock_combine_change_rsquared.append(results.rsquared)
-print(oprt_costock_combine_change_rsquared)
-print(oprt_costock_combine_bench_rsquared)
-
-
-x = ['1d', '7d', '30d', '90d']
-
-plt.plot(x, oprt_costock_combine_change_rsquared, label ="stock change", color ="blue")
-plt.plot(x, oprt_costock_combine_bench_rsquared, label ="Benchmark", color ="green")
-plt.title('oprt R^2 for % change and Benchmark in stock')
-plt.ylabel('R^2')
-plt.grid()
-plt.legend()
-plt.show()
-
-for i in oprt_deals:
-    plt.plot(i['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'])
-    plt.show()
-
-### SOFI ########################################## SOFI ######################################## SOFI ##################
+oprt_files = [oprt_cohort_stock1, oprt_cohort_stock3] #oprt_cohort_stock2
 
 sofi_cohort_stock1 = pd.read_csv(f'Output data/cohort stock/SOFI/file_0_benchmark.csv')
 sofi_cohort_stock2 = pd.read_csv(f'Output data/cohort stock/SOFI/file_1_benchmark.csv')
 sofi_cohort_stock3 = pd.read_csv(f'Output data/cohort stock/SOFI/file_2_benchmark.csv')
-
-upst_deals = [sofi_cohort_stock1, sofi_cohort_stock3] # lc_cohort_stock2
-
-for df in upst_deals:
-    df.columns = df.columns.str.replace(' ', '_')
-    df.columns = df.columns.str.replace('+', '')
-    df['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'] = df['Gross_Coupon'].subtract(df['Annualized_Net_Loss_Rate'])
-    df['d1_dist_from_bench'] = df['adj_1_change'] - df['avg_adj_1_change']
-    df['d7_dist_from_bench'] = df['adj_7_change'] - df['avg_adj_7_change']
-    df['d30_dist_from_bench'] = df['adj_30_change'] - df['avg_adj_30_change']
-    df['d90_dist_from_bench'] = df['adj_90_change'] - df['avg_adj_90_change']
-
-sofi_cohort_stock_combined = pd.concat(upst_deals, ignore_index=True, sort=False)
-
-sofi_costock_combine_bench_rsquared = []
-for i in ['d1_dist_from_bench', 'd7_dist_from_bench', 'd30_dist_from_bench', 'd90_dist_from_bench']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=sofi_cohort_stock_combined).fit()
-    print(results.summary())
-    sofi_costock_combine_bench_rsquared.append(results.rsquared)
-print(sofi_costock_combine_bench_rsquared)
-
-sofi_costock_combine_change_rsquared = []
-for i in ['adj_1_change', 'adj_7_change', 'adj_30_change', 'adj_90_change']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=sofi_cohort_stock_combined).fit()
-    print(results.summary())
-    sofi_costock_combine_change_rsquared.append(results.rsquared)
-print(sofi_costock_combine_change_rsquared)
-print(sofi_costock_combine_bench_rsquared)
-
-
-x = ['1d', '7d', '30d', '90d']
-
-plt.plot(x, sofi_costock_combine_change_rsquared, label ="stock change", color ="blue")
-plt.plot(x, sofi_costock_combine_bench_rsquared, label ="Benchmark", color ="green")
-plt.title('sofi R^2 for % change and Benchmark in stock')
-plt.ylabel('R^2')
-plt.grid()
-plt.legend()
-plt.show()
-
-for i in upst_deals:
-    plt.plot(i['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'])
-    plt.show()
-
-### UPST ########################################## UPST ######################################## UPST ##################
+sofi_files = [sofi_cohort_stock1, sofi_cohort_stock3] # lc_cohort_stock2
 
 upst_cohort_stock1 = pd.read_csv(f'Output data/cohort stock/UPST/file_0_benchmark.csv')
 upst_cohort_stock2 = pd.read_csv(f'Output data/cohort stock/UPST/file_1_benchmark.csv')
 upst_cohort_stock3 = pd.read_csv(f'Output data/cohort stock/UPST/file_2_benchmark.csv')
 upst_cohort_stock4 = pd.read_csv(f'Output data/cohort stock/UPST/file_2_benchmark.csv')
+upst_files = [upst_cohort_stock1, upst_cohort_stock2, upst_cohort_stock3, upst_cohort_stock4]
 
+import itertools
+combined_cohort_deals = list(itertools.chain(afrm_files, lc_files, oprt_files, sofi_files, upst_files))
 
-upst_deals = [upst_cohort_stock1, upst_cohort_stock2, upst_cohort_stock3, upst_cohort_stock4]
+#### Regression runs
+df_combined_files = pd.concat(afrm_files, ignore_index=True, sort=False)
+df_combined_files = add_columns(df_combined_files)
 
-for df in upst_deals:
-    df.columns = df.columns.str.replace(' ', '_')
-    df.columns = df.columns.str.replace('+', '')
-    df['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'] = df['Gross_Coupon'].subtract(df['Annualized_Net_Loss_Rate'])
-    df['d1_dist_from_bench'] = df['adj_1_change'] - df['avg_adj_1_change']
-    df['d7_dist_from_bench'] = df['adj_7_change'] - df['avg_adj_7_change']
-    df['d30_dist_from_bench'] = df['adj_30_change'] - df['avg_adj_30_change']
-    df['d90_dist_from_bench'] = df['adj_90_change'] - df['avg_adj_90_change']
+print('working on adj change')
+y_list = ['d1_dist_from_bench', 'd7_dist_from_bench', 'd30_dist_from_bench', 'd90_dist_from_bench']
+X_list = ['Annualized_Net_Loss_Rate', 'Life_CDR']
+bench_in_sample_r2, out_of_sample_r2 = my_ols(df=df_combined_files, X_list=X_list, y_list=y_list)
 
-upst_cohort_stock_combined = pd.concat(upst_deals, ignore_index=True, sort=False)
+print('working on adj change')
+y_list = ['adj_1_change', 'adj_7_change', 'adj_30_change', 'adj_90_change']
+X_list = ['Annualized_Net_Loss_Rate', 'Life_CDR']
+in_sample_r2, out_of_sample_r2 = my_ols(df=df_combined_files, X_list=X_list, y_list=y_list)
 
-upst_costock_combine_bench_rsquared = []
-for i in ['d1_dist_from_bench', 'd7_dist_from_bench', 'd30_dist_from_bench', 'd90_dist_from_bench']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=upst_cohort_stock_combined).fit()
-    print(results.summary())
-    upst_costock_combine_bench_rsquared.append(results.rsquared)
-print(upst_costock_combine_bench_rsquared)
-
-upst_costock_combine_change_rsquared = []
-for i in ['adj_1_change', 'adj_7_change', 'adj_30_change', 'adj_90_change']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=upst_cohort_stock_combined).fit()
-    print(results.summary())
-    upst_costock_combine_change_rsquared.append(results.rsquared)
-print(upst_costock_combine_change_rsquared)
-print(upst_costock_combine_bench_rsquared)
-
-
-x = ['1d', '7d', '30d', '90d']
-
-plt.plot(x, upst_costock_combine_change_rsquared, label ="stock change", color ="blue")
-plt.plot(x, upst_costock_combine_bench_rsquared, label ="Benchmark", color ="green")
-plt.title('upst R^2 for % change and Benchmark in stock')
-plt.ylabel('R^2')
-plt.grid()
-plt.legend()
-plt.show()
-
-for i in upst_deals:
-    plt.plot(i['Gross_Coupon_Minus_Annualized_Net_Loss_Rate'])
-    plt.show()
-
-#### All cohort deals ####
-
-combined_cohort_deals = [afrm_cohort_stock_combined, upst_cohort_stock_combined, afrm_cohort_stock_combined,
-                   oprt_cohort_stock_combined, lc_cohort_stock_combined]
-
-all_issuers_cohort_stocks = pd.concat(combined_cohort_deals, ignore_index=True, sort=False)
-
-all_costock_combine_bench_rsquared = []
-for i in ['d1_dist_from_bench', 'd7_dist_from_bench', 'd30_dist_from_bench', 'd90_dist_from_bench']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=all_issuers_cohort_stocks).fit()
-    print(results.summary())
-    all_costock_combine_bench_rsquared.append(results.rsquared)
-print(all_costock_combine_bench_rsquared)
-
-all_costock_combine_change_rsquared = []
-for i in ['adj_1_change', 'adj_7_change', 'adj_30_change', 'adj_90_change']:
-    results = smf.ols(f'{i} ~ Gross_Coupon_Minus_Annualized_Net_Loss_Rate + Life_CDR', data=all_issuers_cohort_stocks).fit()
-    print(results.summary())
-    all_costock_combine_change_rsquared.append(results.rsquared)
-print(all_costock_combine_change_rsquared)
-print(all_costock_combine_bench_rsquared)
-
-
-x = ['1d', '7d', '30d', '90d']
-
-plt.plot(x, all_costock_combine_change_rsquared, label ="stock change", color ="blue")
-plt.plot(x, all_costock_combine_bench_rsquared, label ="Benchmark", color ="green")
-plt.title('all R^2 for % change and Benchmark in stock')
-plt.ylabel('R^2')
-plt.grid()
-plt.legend()
-plt.show()
-
-
-
-
-
+plot_r2(in_sample_r2, bench_in_sample_r2)
