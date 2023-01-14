c25b6dafd0715f4c090da7f0e2e858f015e24de3
jy2k <jonathanyarkoni@yahoo.com>
2023-01-14 12:02:06 +0200

    Strategy working with hard coded weight values taking top 1 and bottom 1 from sorted predictions

--- trade_strat.py
+++ trade_strat.py
@@ -41,24 +41,75 @@ from statsmodels.formula.api import ols
 
 ### Step 5 -
 
-df = pd.read_csv('/Users/eyalben-eliyahu/Desktop/mockmock.csv')
+df = pd.read_csv('merged_file.csv')
+df['AFRM_adj_1_change_pred'] = df['AFRM_adj_1_change_pred'].ffill()
+df['AFRM_adj_1_change_pred'] = df['AFRM_adj_1_change_pred'].bfill()
+df['UPST_adj_1_change_pred'] = df['UPST_adj_1_change_pred'].ffill()
+df['UPST_adj_1_change_pred'] = df['UPST_adj_1_change_pred'].bfill()
+df['SOFI_adj_1_change_pred'] = df['SOFI_adj_1_change_pred'].ffill()
+df['SOFI_adj_1_change_pred'] = df['SOFI_adj_1_change_pred'].bfill()
+df['LC_adj_1_change_pred'] = df['LC_adj_1_change_pred'].ffill()
+df['LC_adj_1_change_pred'] = df['LC_adj_1_change_pred'].bfill()
+df['OPRT_adj_1_change_pred'] = df['OPRT_adj_1_change_pred'].ffill()
+df['OPRT_adj_1_change_pred'] = df['OPRT_adj_1_change_pred'].bfill()
+# TODO: remove this filtering line
+df = df.iloc[300:350]
+
 df['long_gain'] = 0
 df['short_gain'] = 0
+df['daily_return'] = 0
+#at this point the DAtaframe is ready
+#now need to fill in each long_gain and short_gain prediction
 
-def trade (df):
-    for row in df.iterrows():
-        tes = row[1]['LC_adj_1_change_pred']
-        my_list = [{'name': 'LC', 'pred': row[1]['LC_adj_1_change_pred'], 'actual': row[6]['LC_adj_1_change']},
-                   {'name': 'UPST', 'pred': row[2]['UPST_adj_1_change_pred'], 'actual': row[7]['UPST_adj_1_change']},
-                   {'name': 'SOFI', 'pred': row[3]['SOFI_adj_1_change_pred'], 'actual': row[8]['SOFI_adj_1_change']},
-                   {'name': 'AFRM', 'pred': row[4]['AFRM_adj_1_change_pred'], 'actual': row[9]['AFRM_adj_1_change']},
-                   {'name': 'OPRT', 'pred': row[5]['OPRT_adj_1_change_pred'], 'actual': row[10]['OPRT_adj_1_change']}]
-        sorted_list = sorted(my_list, key=lambda x: x['pred'])
-        df['long_gain'] = (1 * (sorted_list[0]['actual'] + sorted_list[1]['actual']))
-        df['short_gain'] = (-1 * (sorted_list[4]['actual'] + sorted_list[3]['actual']))
+for index, row in df.iterrows():
+    #print(index, row['Date'], row['AFRM_adj_1_change'], row['AFRM_adj_1_change_pred'])
+    #Create a dictionary from the row
+    actual = {
+        'AFRM_adj_1_change': row['AFRM_adj_1_change'],
+
+        'LC_adj_1_change': row['LC_adj_1_change'],
+
+        'OPRT_adj_1_change': row['OPRT_adj_1_change'],
+
+        'SOFI_adj_1_change': row['SOFI_adj_1_change'],
+
+        'UPST_adj_1_change': row['UPST_adj_1_change'],
+    }
+
+    predicted = {
+        'AFRM_adj_1_change_pred': row['AFRM_adj_1_change_pred'],
+
+        'LC_adj_1_change_pred': row['LC_adj_1_change_pred'],
+
+        'OPRT_adj_1_change_pred': row['OPRT_adj_1_change_pred'],
+
+        'SOFI_adj_1_change_pred': row['SOFI_adj_1_change_pred'],
+
+        'UPST_adj_1_change_pred': row['UPST_adj_1_change_pred'],
+    }
+
+    sorted_d = sorted(predicted.items(), key=lambda x: x[1])
+
+    #get 2 max
+    max_key = sorted_d[-1][0]
+    max_2_key = sorted_d[-2][0]
+
+    #get 2 min
+    min_key = sorted_d[0][0]
+    min_2_key = sorted_d[1][0]
+
+    #creating the strat assuming weights of 0.5 -0.5
+    print(index)
+    df.at[index, 'long_gain'] = 0.5 * (actual[max_key[:-5]])
+    df.at[index, 'short_gain'] = -0.5 * (actual[min_key[:-5]])
+
+    df.at[index, 'daily_return'] = df.at[index, 'long_gain'] + df.at[index, 'short_gain']
+
+    print('test')
+
+print('end')
 
 
-trade(df)
 
 
 
