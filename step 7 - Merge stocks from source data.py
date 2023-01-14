fcb66ea51f053649c9678059dd94696221112794
jy2k <jonathanyarkoni@yahoo.com>
2023-01-14 13:01:02 +0200

    Cahnged file names and organized. Step 6-8 now run consecutively

--- step 7 - Merge stocks from source data.py
+++ step 7 - Merge stocks from source data.py
@@ -77,3 +77,20 @@ result4 = pd.merge(result3, df5, on='Date')
 
 # Save the merged DataFrame to a new CSV file
 result4.to_csv('merged_all_stocks_source_data.csv', index=False)
+
+
+# Create a list of 5 items
+stock_list = ['AFRM', 'LC', 'OPRT', 'SOFI', 'UPST']
+
+df_merged = result4
+
+# Loop over the list
+for stock in stock_list:
+    print(stock)
+
+    df_current_stock = pd.read_csv(str(stock+"_preds.csv"))
+
+    df_merged = pd.merge(df_current_stock, df_merged, on="Date", how="outer")
+
+# Write the merged dataframe to a new CSV file
+df_merged.to_csv("merged_file.csv", index=False)
